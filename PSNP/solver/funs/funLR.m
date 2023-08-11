3215ff96674fd101f6d28c295ed1b15e86cdcad5
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2023-08-11 23:40:34 +0800

    Update funLR.m

--- PSNP/solver/funs/funLR.m
+++ PSNP/solver/funs/funLR.m
@@ -1,40 +1,43 @@
 function [out1,out2] = 	funLR(xT,T,key,mu, data)
 
-m     = length(data.b);    
-if  isempty(T) 
-    switch key
-        case 'f'
-             out1 = log(2);   
-        case 'g'
-             out1 = ((1/2-data.b)'*data.A)'/m; 
-        case 'gh'
-             out1 = ((1/2-data.b)'*data.A)'/m; 
-             out2 = [];
+    m  = length(data.b);  
+    if isfield(data,'At') 
+        Atb = @(v)data.At*v;
+    else 
+        Atb = @(v)(v'*data.A)';
     end
-else
-    AT  = data.A(:,T);
-    Ax  = AT*xT; 
-    eAx = exp(Ax);  
-    switch key
-        case 'f'
+    
+    if  isempty(T) 
+        if isequal(key, 'f')
+            out1 = log(2);  
+        else
+            out1 = Atb(1/2-data.b)/m;  
+            if  isequal(key, 'gh')
+                out2 = [];
+            end
+        end
+    else 
+        AT  = data.A(:,T);
+        Ax  = AT*xT; 
+        eAx = exp(Ax);  
+        if isequal(key, 'f')
             if sum(eAx)==Inf 
                 obj  = sum(log(1+eAx(Ax<=300)))+sum(Ax(Ax>300))-sum(data.b.*Ax);  
             else
-                obj  = sum(log(1+eAx)-data.b.*Ax);    % objective function 
+                obj  = sum(log(1+eAx)-data.b.*Ax);     % objective function 
             end   
             out1    = obj/m + mu*norm(xT,'fro')^2/2;
-        case 'g'
+        else
             dAx     = 1./(1+eAx);
-            out1    = ((1-data.b-dAx)'*data.A)'/m;     % gradient   
-            out1(T) =  out1(T) + mu*xT;
-        case 'gh'
-            dAx     = 1./(1+eAx);
-            out1    = ((1-data.b-dAx)'*data.A)'/m;     % gradient
-            out1(T) =  out1(T) + mu*xT;          
-            d       = dAx.*(1-dAx)/m;          
-            out2    =  @(v) ( ((d.*(AT*v))'*AT)' + mu*v) ;  % Hessian                  
-    end            
-end
-    
+            dAx1    = 1-dAx;
+            out1    = Atb(dAx1-data.b)/m;                    
+            out1(T) = out1(T) +  mu*xT;                        % gradient
+            
+            if  isequal(key, 'gh')
+                d    = (dAx.*dAx1)/m;          
+                out2 =  @(v) ( ((d.*(AT*v))'*AT)' + mu*v) ;       % Hessain
+            end                
+        end            
+    end        
 end
 
