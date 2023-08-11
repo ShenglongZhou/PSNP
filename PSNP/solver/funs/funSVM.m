f1d125b7b429368190a48339e376d0177e4d1c68
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2023-08-11 23:40:54 +0800

    Update funSVM.m

--- PSNP/solver/funs/funSVM.m
+++ PSNP/solver/funs/funSVM.m
@@ -1,45 +1,40 @@
 function [out1,out2] = 	funSVM(xT,T,key,mu, data)
  
     m = length(data.b);
-
+    if isfield(data,'At') 
+        Atb   = @(v,ind)data.At(:,ind)*v;
+    else 
+        Atb   = @(v,ind)(v'*data.A(ind,:))';
+    end
+    
     if  isempty(T) 
-        switch key
-            case 'f'
-                 out1 = 1/2;   
-            case 'g'
-                 out1 = -(data.b'*data.A)'/m; 
-            case 'gh'
-                 out1 = -(data.b'*data.A)'/m; 
-                 out2 = [];
+        if isequal(key, 'f')
+            out1 = 1/2;   
+        else
+            out1 = -Atb(data.b,1:m)/m; 
+            if  isequal(key, 'gh')
+                out2 = [];
+            end
         end
-    else
-     
+    else 
         AT   = data.A(:,T);
         bAx  = 1-data.b.*(AT*xT);
-        Tp   = (bAx>0);
+        Tp   = (bAx>0); 
         bAxT = bAx(Tp); 
-        switch key
-            case 'f'  
-                out1    = mu*norm( xT,'fro')^2/2 + norm(bAxT,'fro')^2/2/m;
-            case 'g'  
-                out1    = -( (bAxT.*data.b(Tp))'*data.A(Tp,:) )'/m;     % gradient   
-                out1(T) =  out1(T) +  mu*xT;
-            case 'gh'
-                out1    = -( (bAxT.*data.b(Tp))'*data.A(Tp,:) )'/m;     % gradient   
-                out1(T) =  out1(T) +  mu*xT;
-
+        if isequal(key, 'f')
+            out1 = mu*norm( xT,'fro')^2/2 + norm(bAxT,'fro')^2/2/m;     % objective
+        else
+            out1    = - Atb(bAxT.*data.b(Tp),Tp)/m;                     
+            out1(T) =  out1(T) +  mu*xT;                                % gradient           
+            if  isequal(key, 'gh')
                 s       = nnz(T); 
-                ATpT    = AT(Tp,:);              
-                if s    < .100 && nnz(Tp) < 100
+                ATpT    = AT(Tp,:);         
+                if s    < 100 && nnz(Tp) < 100
                    out2 =  mu*speye(s) + (ATpT'*ATpT)/m;                % Hessian
                 else
                    out2 =  @(v) (  mu*v + ((ATpT*v)'*ATpT)'/m ) ;  
                 end   
-                
+            end                
         end            
     end
 end
-
- 
-
-
