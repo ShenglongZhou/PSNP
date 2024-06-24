999dfda2c4d729f3178739533256ae4802598a5b
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2024-06-24 11:48:40 +0800

    Update PSNP.m

--- PSNP/solver/PSNP.m
+++ PSNP/solver/PSNP.m
@@ -11,8 +11,9 @@ function Out = PSNP(funcF,n,lambda,q,pars)
 %     lambda : Penalty parameter                                             (required) 
 %     q      : one of {0, 1/2, 2/3}                                          (required)
 %     pars:  Parameters are all OPTIONAL
-%            pars.x0      --  Starting point of x,   pars.x0=zeros(n,1) (default)
+%            pars.x0      --  Starting point of x,   pars.x0 = zeros(n,1) (default)
 %            pars.prob    --  ='CS', 'SVM, or 'LR'  
+%            pars.cond    --  Conditional or unconditional Semismooth newton method  (default, 1)  
 %            pars.show    --  Display results or not for each iteration (default, 1)
 %            pars.maxit   --  Maximum number of iterations, (default,2000) 
 %            pars.tol     --  Tolerance of the halting condition, (default,1e-6)
@@ -37,11 +38,11 @@ if  nargin < 4
 end
 if nargin < 5; pars = [];  end 
 
-[sig1,sig2,q1,q2,lamq,lamq1,alpha0,cg_tol,cg_it,x0,tol,...
-          maxit,newton,show,prob] = setparameters(n,lambda, q, pars);
+[sig1,sig2,q1,q2,lamq,lamq1,alpha0,cg_tol,cg_it,x0,tol,maxit,newton,...
+         show,prob,cond,rate,i0] = setparameters(n, lambda, q, pars);
 Fnorm    = @(var)norm(var,'fro')^2; 
 Funcs    = @(x,T,key)funcF(x,T,key);
-ProxLq   = @(x,t)ProxmLq(x,t,q);
+ProxLq   = @(x,t)ProxmaLq(x,t,q);
 Qnorm    = @(absvar)sum(absvar.^q); 
 GQnorm   = @(var,absvar)(sign(var).*absvar.^q1); 
 HQnorm   = @(absvar)(absvar.^q2); 
@@ -50,9 +51,11 @@ x        = x0;
 T        = find(x~=0); 
 sT       = nnz(T);
 w        = x(T);
-if sT
-    Obj  = Funcs(w,T,'f');
-    grad = Funcs(w,T,'g'); 
+if sT       > 0
+    absw    = abs(w);
+    Obj     = Funcs(w,T,'f') + lambda*Qnorm(absw); 
+    grad    = Funcs(w,T,'g');
+    grad(T) = grad(T) + lamq*GQnorm(w,absw);
 else
     Obj  = Funcs(x,[],'f');
     grad = Funcs(x,[],'g'); 
@@ -61,29 +64,24 @@ end
 sT       = nnz(T);
 Error    = zeros(maxit,1); 
 if  show 
-    fprintf(' Start to run the solver -- PSNP with q = %5.3f \n',q);
-    fprintf(' ----------------------------------------------------------------------------\n');
-    fprintf(' Iter      ErrGrad        ErrProx        Objective      Sparsity     Time(sec) \n'); 
-    fprintf(' ----------------------------------------------------------------------------\n');
-    fprintf('%4d       %5.2e      %8.2e       %8.5e    %7d      %8.3f\n',...
-                0, norm(grad,'inf'), 1, Obj, sT, toc(t0)); 
+    if  cond 
+        fprintf(' \n Start to run the solver -- PCSNP with q = %5.3f \n',q);
+    else
+        fprintf(' Start to run the solver -- PSNP with q = %5.3f \n',q);
+    end
+    fprintf(' ---------------------------------------------------------------------\n');
+    fprintf(' Iter      ErrGrad          Objective        Sparsity        Time(sec) \n'); 
+    fprintf(' ---------------------------------------------------------------------\n');
+    fprintf('%4d       %5.2e        %8.5e      %7d         %8.3f\n',...
+                0, norm(grad,'inf'), Obj, sT, toc(t0)); 
 end
 
 % The main body
 for iter = 1:maxit
-          
-    switch prob
-        case 'CS';  rate = 0.5; i0 = 6;
-        case 'SVM'; rate = 0.1 + 0.1*(iter>5);
-                    i0   = 5.0 + 5.0*(iter>5); 
-        case 'LR';  rate = .25 + .25*(iter>5);
-                    i0   =  15 + 10* (iter>5);       
-        otherwise;  rate = 0.5; i0 = 25;
-    end
-   
+
     alpha       = alpha0; 
     Told        = T;
-    for i       = 1:i0 
+    for i       = 1:i0  
         [w,T]   = ProxLq(x-alpha*grad,alpha*lambda);        
         absw    = abs(w);  
         Objw    = Funcs(w,T,'f')+lambda*Qnorm(absw);  
@@ -111,35 +109,40 @@ for iter = 1:maxit
        ident = nnz(T-Told)==0;   
     end     
  
-    if  newton && sT>0 &&  (sT <0.5*n || ident ==1) && (i~=i0 || iter>=5)
+    if cond
+       switchon = (ident ==1);
+    else
+       switchon = (sT < 0.25*n || ident ==1) && (i~=i0 || iter>=5);
+    end
+ 
+    if  newton && sT>0 && switchon
         [grad,Hess] = Funcs(w,T,'gh'); 
         gradT       = grad(T);
         if q        > 0
             gradT   = gradT + lamq*GQnorm(w,absw);
             dw      = lamq1*HQnorm(absw);
             if isa(Hess, 'function_handle') 
-                Hess = @(v)(Hess(v)+dw.*v);
+                Hess = @(v)(Hess(v) + dw.*v);
             else
                 Hess(1:(sT+1):end) = Hess(1:(sT+1):end) + dw';
             end
         end
         if  isa(Hess, 'function_handle')    
-            %cg_tol = 1e-16*sT; 
             if mod(iter,10)==0  
                 cg_tol = max(cg_tol/10,1e-15*sT);
                 cg_it  = min(cg_it+5,25);
             end    
-            d   = my_cg(Hess,gradT,cg_tol,cg_it,zeros(sT,1));  
+            d    = my_cg(Hess,gradT,cg_tol,cg_it,zeros(sT,1));  
         else 
             d    = Hess\gradT; 
         end
         
-        beta   = 1;
-        Fd      = Fnorm(d);   
-        for j   = 1 : 10
-            v       = w - beta* d;
-            absv    = abs(v);
-            Objv    = Funcs(v,T,'f') + lambda*Qnorm(absv);          
+        beta     = 1;
+        Fd       = Fnorm(d);   
+        for j    = 1 : 5
+            v    = w - beta* d;
+            absv = abs(v);
+            Objv = Funcs(v,T,'f') + lambda*Qnorm(absv);          
             if  Objv <= Obj - sig2*beta^2*Fd 
                 x(T) = v; 
                 w    = v; 
@@ -162,9 +165,13 @@ for iter = 1:maxit
         lamq     = lambda*q;
         lamq1    = lamq*q1;
         if  isequal(prob,'SVM')
-            x    = zeros(n,1);
-            Obj  = Funcs(w,[],'f');
-            grad = Funcs(w,[],'g');
+            x       = [zeros(n-1,1);1];
+            w       = 1;
+            absw    = abs(w);
+            T       = n;
+            Obj     = Funcs(w,T,'f') +  lambda*Qnorm(absw);
+            grad    = Funcs(w,T,'g'); 
+            grad(T) = grad(T) + lamq*GQnorm(w,absw);
         end
     else
         ErrGradT = norm(gradT,'inf'); 
@@ -172,21 +179,21 @@ for iter = 1:maxit
     ErrObj      = abs(Obj-Objold)/(1+abs(Obj));    
     Error(iter) = ErrGradT ;
     if  show  && mod(iter,Count(iter))==0
-        fprintf('%4d       %5.2e      %8.2e       %8.5e    %7d      %8.3f\n',...
-                iter,   ErrGradT, ErrObj, Obj, sT, toc(t0)); 
+        fprintf('%4d       %5.2e        %8.5e      %7d         %8.3f\n',...
+                iter, ErrGradT, Obj, sT, toc(t0)); 
     end
              
     % Stopping criteria
      if  ident && max(ErrGradT,ErrObj) <tol
          if  show  && mod(iter,Count(iter))~=0
-                fprintf('%4d       %5.2e      %8.2e       %8.5e    %7d      %8.3f\n',...
-                iter,   ErrGradT, ErrObj, Obj, sT, toc(t0)); 
+             fprintf('%4d       %5.2e        %8.5e      %7d         %8.3f\n',...
+             iter, ErrGradT, Obj, sT, toc(t0));
          end
          break;  
      end
 end
 
-fprintf(' ----------------------------------------------------------------------------\n');
+fprintf(' ---------------------------------------------------------------------\n');
 Out.time    = toc(t0);
 Out.iter    = iter;
 Out.sol     = x;
@@ -194,15 +201,17 @@ Out.obj     = Obj;
 Out.Error   = Error; 
 end
 
-function [sig1,sig2,q1,q2,lamq,lamq1,alpha0,cg_tol,cg_it,x0,tol,...
-          maxit,newton,show,prob] = setparameters(n,lambda, q, pars)
+% Set up parameters -------------------------------------------------------
+function [sig1,sig2,q1,q2,lamq,lamq1,alpha0,cg_tol,cg_it,x0,tol,maxit,...
+          newton,show,prob,cond,rate,i0] = setparameters(n, lambda, q, pars)
 
 if isfield(pars,'x0');     x0     = pars.x0;     else; x0 = zeros(n,1); end 
 if isfield(pars,'tol');    tol    = pars.tol;    else; tol    = 1e-6;   end  
 if isfield(pars,'maxit');  maxit  = pars.maxit;  else; maxit  = 1e4;    end
 if isfield(pars,'newton'); newton = pars.newton; else; newton = 1;      end 
 if isfield(pars,'show');   show   = pars.show;   else; show   = 1;      end 
-if isfield(pars,'prob');   prob   = pars.prob;   else; prob   = 'None'; end 
+if isfield(pars,'prob');   prob   = pars.prob;   else; prob   = 'None'; end
+if isfield(pars,'cond');   cond   = pars.cond;   else; cond   = 1;      end 
 
 sig1  = 1e-6; 
 sig2  = 1e-10;
@@ -215,51 +224,26 @@ switch prob
     case 'CS';   alpha0 = 1-q/2;
                  cg_tol = 1e-8; 
                  cg_it  = 10;
-    case 'SVM';  alpha0 = 20;
-                 cg_tol = 1e-6; 
-                 cg_it  = 5;
+                 rate   = 0.5; 
+                 i0     = 6;
+    case 'SVM';  alpha0 = 10;
+                 cg_tol = 1e-8; 
+                 cg_it  = 10;
+                 rate   = 0.5; 
+                 i0     = 25;
    case 'LR';    alpha0 = max(1e4,10*sqrt(n));
                  cg_tol = 1e-6; 
-                 cg_it  = 5;             
+                 cg_it  = 5; 
+                 rate   = 0.5; 
+                 i0     = 25;
     otherwise;   alpha0 = 1;
                  cg_tol = 1e-8; 
                  cg_it  = 10;
+                 rate   = 0.5; 
+                 i0     = 25;
 end
 end
-% proximal operator of Lq norm ---------------------------------------------
-function [px,T] = ProxmLq(a,alam,q)
-
-switch q
-    case 0
-         t     = sqrt(2*alam);
-         T     = find(abs(a)>t);  
-         px    = a(T);
-         
-    case 1/2 
-         t     = (3/2)*alam^(2/3);
-         T     = find(abs(a) > t);
-         aT    = a(T);
-         phi   = acos( (alam/4)*(3./abs(aT)).^(3/2) );
-         px    = (4/3)*aT.*( cos( (pi-phi)/3) ).^2;
-         % px    = (2/3)*aT.*(1+ cos( 2*pi/3-(2/3)*phi ) );
-        
-    case 2/3
-         t     = (128/27)^(1/4)*(alam)^(3/4);
-         T     = find( abs(a) >  t );  
-         aT    = a(T);       
-         tmp1  = aT.^2/2; 
-         tmp2  = sqrt( tmp1.^2 - (8*alam/9)^3 );  
-         phi   = (tmp1+tmp2).^(1/3)+(tmp1-tmp2).^(1/3);
-         px    = sign(aT)/8.*( sqrt(phi)+sqrt(2*abs(aT)./sqrt(phi)-phi) ).^3; 
-      %  px  =  real(px);
-    otherwise
-        fprintf(' Input ''q'' is incorrect!!! \n');
-        fprintf(' Please re-enter ''q'' one value of {0,1/2,2/3}!!!\n')
-end
-
-end
-
-
+ 
 % conjugate gradient-------------------------------------------------------
 function x = my_cg(fx,b,cgtol,cgit,x)
     if ~isa(fx,'function_handle'); fx = @(v)fx*v; end
