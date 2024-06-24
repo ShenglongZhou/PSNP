9170a84f5d79d4da1f745a922b148941ebd2662f
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2024-06-24 11:46:49 +0800

    Update demonSVM.m

--- PSNP/demonSVM.m
+++ PSNP/demonSVM.m
@@ -1,30 +1,25 @@
 % demon sparse support vector machine problems 
-clc; close all; warning off; addpath(genpath(pwd));
+clc; close all; clear all; warning off; 
+addpath(genpath(pwd));
 
 dat     = load('arcene.mat'); 
 class   = load('arceneclass.mat');  
 b       = class.y;
 b( b   ~= 1) = -1;
-data.A  = [normalization(dat.X,2)  ones(size(b))];
+data.A  = [normalization(dat.X,1)  ones(size(b))];
 data.b  = b;
 [m,n]   = size(data.A); 
-data.At = data.A';
- 
-pars.prob  = 'SVM';
-pars.tol   = 1e-5*log2(m*n);
-q0         = [0 1/2 2/3];
-lam        = @(q)3e-4*(1+6*q)*log2(n/m)*norm(data.b'*data.A,'inf')/m;
-out        = cell(length(q0),1);
-for i      = 1:length(q0) 
-    lambda = lam(q0(i));
-    func   = @(xT,T,key)funSVM(xT,T,key,lambda,data);
-    out{i} = PSNP(func,n,lambda,q0(i),pars);   
-end
+  
+pars.prob = 'SVM';
+pars.cond = 0;
+pars.x0   = [zeros(n-1,1);1];
+q         = 0;
+lam       = 0.4*norm(data.b'*data.A,'inf')/m/log10(10+n);
+func      = @(xT,T,key)funSVM(xT,T,key,20/m,data);
+out       = PSNP(func,n,lam,q,pars);   
 
 acc = @(v)( 1-nnz(data.b - sign( data.A(:,v~=0)*v(v~=0) ))/m );
-fprintf('   q      Objective     Accuracy     Time(sec)\n');
-fprintf(' ---------------------------------------------\n');
-for i  = 1:length(q0)
-    fprintf('%6.3f     %5.2e      %5.2f%%      %7.3f    \n', ...
-    q0(i),out{i}.obj,acc(out{i}.sol)*100,out{i}.time);
-end
+fprintf('\n    q      Objective     Accuracy      Sparsity     Time(sec)\n');
+fprintf(' -------------------------------------------------------------\n');
+fprintf(' %6.3f    %5.2e      %5.2f%%      %6d       %7.3f    \n', ...
+    q,out.obj,acc(out.sol)*100, nnz(out.sol), out.time);
